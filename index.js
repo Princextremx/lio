a315e448b531fb3db15f4866aa354a5310b745b2
MRSID508 <sidmr771@gmail.com>
2025-07-23 20:43:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,8 @@ const port = process.env.PORT || 9090;
       
   conn.ev.on('connection.update', (update) => {
   const { connection, lastDisconnect } = update
-  if (connection === 'close'){  (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+  if (connection === 'close') {
+  if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
   connectToWA()
   }
   } else if (connection === 'open') {
