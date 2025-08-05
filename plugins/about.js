f855723101f8226d75fa46179b16e495676041e1
Princextremx <pxtreme86@gmail.com>
2025-08-05 11:06:03 -0600

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -1,33 +1,28 @@
 const config = require('../config')
+const { runtime } = require('../lib/functions');
 const {cmd , commands} = require('../command')
 cmd({
-    pattern: "about",
-    alias: "dev",
-    react: "👑",
+    pattern: "bot",
+    alias: "xtreme",
+    react: "🤖",
     desc: "get owner dec",
-    category: "main",
+    category: "misc",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let about = `> *_ᴛʜᴇ ʙᴇsᴛ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ_*
-╭─ 「 *\`MINI XTREME\`* 」
-│❍ *ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*
-│❍ *ʀᴇᴀʟ ɴᴀᴍᴇ➭ sɪᴅᴅʜᴀʀᴛʜs*
-│❍ *ɴɪᴄᴋɴᴀᴍᴇ➮ ᴘʀɪɴᴄᴇ sɪᴅ*
-│❍ *ᴀɢᴇ➭ ɴᴏᴛ ᴅᴇғɪɴᴇᴅ*
-│❍ *ᴄɪᴛʏ➭ ɴᴏᴛ ᴅᴇғɪɴᴇᴅ*
-│❍ *ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ*
-╰────────────────❍
-╭─ 「 *\`MINI DEV\`* 」
-│❍➳ *ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*
-│❍➳ *ᴏɴʟʏ ᴏɴᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ*
-│❍➳ *ʙᴏᴛ➭ xᴛʀᴇᴍᴇ xᴍᴅ*
-│❍➳ *ᴏᴡ➭* +528145550802
+    const uptime = runtime(process.uptime());
+    const startTime = new Date(Date.now() - process.uptime() * 1000);
+let about = `╭─ 「 *\`BOT ONLINE\`* 」
+│꙳ *ʙᴏᴛ ɴᴀᴍᴇ* ↔ xᴛʀᴇᴍᴇ xᴍᴅ
+│꙳ *sᴛᴀᴛᴜs* ↔ ᴏɴʟɪɴᴇ
+│꙳ *ᴀᴜᴛᴏ ʀᴇsᴛᴀʀᴛ* ↔ ᴏɴʟɪɴᴇ
+│꙳ *ʙᴏᴛ ʀᴜɴ* ↔ ${uptime}
+│꙳ *ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ*
 ╰────────────────❍
 > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*`
 await conn.sendMessage(from, {
-    image: { url: 'https://files.catbox.moe/p01dj2.jpg' },
+    image: { url: 'https://files.catbox.moe/mry39g.jpg' },
     caption: about,
     contextInfo: {
         mentionedJid: [m.sender],
@@ -35,7 +30,7 @@ await conn.sendMessage(from, {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363418161689316@newsletter', // ou ton JID actuel
-            newsletterName: '𝐌𝐈𝐍𝐈-𝐗𝐓𝐑𝐄𝐌𝐄',
+            newsletterName: '𝗫𝗧𝗥𝗘𝗠𝗘-𝗫𝗠𝗗',
             serverMessageId: 143
         }
     }
