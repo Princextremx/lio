d2040a36095b03da5f1ab139d767f01e338a9797
Princextremx <pxtreme86@gmail.com>
2025-08-06 05:28:47 -0600

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,24 +1,26 @@
-const { cmd } = require("../command");  
-const { sleep } = require("../lib/functions");  
 
-cmd({  
-    pattern: "restart",  
-    desc: "*ʀᴇsᴛᴀʀᴛ xᴛʀᴇᴍᴇ xᴍᴅ*",  
-    category: "owner",  
-    filename: __filename  
-},  
-async (conn, mek, m, { reply, isCreator }) => {  
-    try {  
-        if (!isCreator) {  
-            return reply("*ᴏɴʟʏ ᴛʜᴇ ʙᴏᴛ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.*");  
-        }  
 
-        const { exec } = require("child_process");  
-        reply("*ᴍɪɴɪ ʙᴏᴛ ʀᴇsᴛᴀʀᴛɪɴɢ...*");  
-        await sleep(1500);  
-        exec("pm2 restart all");  
-    } catch (e) {  
-        console.error(e);  
-        reply(`${e}`);  
-    }  
-});
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const {sleep} = require('../lib/functions')
+
+cmd({
+    pattern: "restart",
+    alias: ["rebot","reboot"], 
+    react: "🔄",
+    desc: "restart the bot",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (!isOwner) return;
+const {exec} = require("child_process")
+reply("restarting...")
+await sleep(1500)
+exec("pm2 restart all")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
